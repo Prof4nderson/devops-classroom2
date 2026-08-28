4adf81da066900e11c77e61fe3ff4a0cb1e6f82b
Prof4nderson <you@example.com>
2026-08-28 05:22:59 -0300

    first commit

--- backend/src/main/java/com/devopsclassroom/entity/TipoUsuario.java
+++ backend/src/main/java/com/devopsclassroom/entity/TipoUsuario.java
@@ -1,7 +1,16 @@
 package com.devopsclassroom.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum TipoUsuario {
-    ALUNO,
-    PROFESSOR,
-    AI, ADMIN
+    ADMIN,
+    USER;
+
+    @JsonCreator
+    public static TipoUsuario fromValue(String value) {
+        if (value == null) {
+            return null;
+        }
+        return TipoUsuario.valueOf(value.trim().toUpperCase());
+    }
 }
